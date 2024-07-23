303b65918d8af4c00ce5fdef7e61b613d52a5b11
mayconxrz@gmail.com <mayconxrz@gmail.com>
2024-07-22 20:01:39 -0400

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  experimental: {
+    cpus: 4
+  },
+  devIndicators: {
+    autoPrerender: false
+  }
+};
 
 export default nextConfig;
